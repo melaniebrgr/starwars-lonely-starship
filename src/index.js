c233a01ac856b70b3ece8e207de0eaccf12f15f7
Melanie Burger <melanie.brgr@gmail.com>
2017-01-29 12:40:39 -0500

    initial routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(
-  <App />,
+import App from './components/App/App';
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
+),
   document.getElementById('root')
 );

2a1503741213e6e5482fec67e6597b26ec1a7b1b
Melanie Burger <melanie.brgr@gmail.com>
2017-01-29 19:00:34 -0500

    move SWAPI fetch logic to App container

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,51 +2,38 @@ import React, { Component } from 'react';
 
 import List from '../List/List';
 
-import 'whatwg-fetch';
-
 export default class Search extends Component {
   constructor() {
     super();
     this.state = {
-      starships: null,
       starshipQuery: ""
     }
   }
-  
-  BASE_URL = 'https://swapi.co/api/starships/';
-  STARSHIP_SEARCH_URL_GENERATOR = starship => `${this.BASE_URL}?search=${starship}`;
 
   updateStarshipQuery(e) {
     const query = e.target.value;
     this.setState({ starshipQuery: query });
   }
 
-  handleKeyPress(e) {
+  handleEnterKeyPress(e) {
     const key = e.key;
-    if (key === 'Enter') this.searchForStarship(this.state.starshipQuery);
-  }
-  
-  searchForStarship(query) {
-    const URL = this.STARSHIP_SEARCH_URL_GENERATOR(query);
-    fetch(URL)
-      .then( response => response.json() )
-      .then( responseData => this.setState({ starships: responseData.results }) );
+    if (key === 'Enter') this.props.searchForStarship(this.state.starshipQuery);
   }
 
-  displayQueryResponce(starships) {
+  displayQueryResponse(starships) {
     const isStarships = starships.length > 0;
-    return isStarships ? <List starships={this.state.starships} /> : <p>{`Sorry, no results found for "${this.state.starshipQuery}"`}</p>;
+    return isStarships ? <List starships={starships} /> : <p>{`Sorry, no results found for "${this.state.starshipQuery}"`}</p>;
   }
 
   render() {
     return (
       <div>
         <label>Search for starship:&nbsp;
-          <input onChange={ this.updateStarshipQuery.bind(this) } onKeyPress={ this.handleKeyPress.bind(this) } type="text" placeholder={`e.g. "Death Star"`} />
+          <input onChange={ this.updateStarshipQuery.bind(this) } onKeyPress={ this.handleEnterKeyPress.bind(this) } type="text" placeholder={`e.g. "Death Star"`} />
         </label>
-        <button onClick={ this.searchForStarship.bind(this, this.state.starshipQuery) } type="search">Search</button>
+        <button onClick={ () => this.props.searchForStarship(this.state.starshipQuery) } type="search">Search</button>
         <h3>Search results:</h3>
-        {this.state.starships ? this.displayQueryResponce(this.state.starships) : <p>Nothing here yet...</p>}
+        {this.props.starships ? this.displayQueryResponse(this.props.starships) : <p>Nothing here yet...</p>}
       </div>
     );
   }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
+import 'whatwg-fetch';
+
 import Search from '../../components/Search/Search';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      starships: null
+    }
+  }
+  
+  BASE_URL = 'https://swapi.co/api/starships/';
+  STARSHIP_SEARCH_URL_GENERATOR = starship => `${this.BASE_URL}?search=${starship}`;
+  
+  searchForStarship(query) {
+    const URL = this.STARSHIP_SEARCH_URL_GENERATOR(query);
+    fetch(URL)
+      .then( response => response.json() )
+      .then( responseData => this.setState({ starships: responseData.results }) );
+  }
+
+  // renderChildrenWithProps() {
+  //   return React.Children.map(this.props.children,
+  //     child => React.cloneElement(child, {
+  //       starships: this.state.starships
+  //     })
+  //   );
+  // }  
+
   render() {
     return (
       <div>
         <h1>A Guide to the Starships of Star Wars</h1>
-        <Search />
+        <Search
+          searchForStarship={this.searchForStarship.bind(this)}
+          starships={this.state.starships}
+        />
         {this.props.children}
       </div>
     );

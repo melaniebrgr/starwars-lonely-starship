7b26e4de3c81a34c3fb5b6d92cc9d7ad566ffccb
Melanie Burger <melanie.brgr@gmail.com>
2017-02-01 18:21:36 -0500

    stub test for Link in List

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { expect, assert } from 'chai';
 import { shallow } from 'enzyme';
 
@@ -8,8 +9,15 @@ describe('<List />', () => {
 
   it('should create a list', () => {
     const wrapper = shallow(<List starships={[{ name: "Death Star" }, { name: "X-wing" }]} />);
+
     assert.isOk(wrapper.children().length, 'List not found');
     expect(wrapper.find('li')).to.have.length(2);
   });
-  
+
+  // TK: How do I test if contains another component?
+  // it('should contain Link components', () => {
+  //   const wrapper = shallow(<List starships={[{ name: "Death Star" }]} />);
+
+  //   expect(wrapper.containsMatchingElement(<Link />)).to.equal(true);
+  // });
 });

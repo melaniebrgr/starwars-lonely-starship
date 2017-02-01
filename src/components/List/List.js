b3289c63dcd146566bd52144b7b2d662e86c4b0f
Melanie Burger <melanie.brgr@gmail.com>
2017-02-01 17:04:35 -0500

    List PropTypes

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-export default function({ starships }) {
+export default function List({ starships }) {
   return (
     <ul>
       {starships.map( (starship, index) => <li key={index}><Link to={`/starship/${starship.name}`}>{starship.name}</Link></li> )}
     </ul>
   );
+}
+
+List.propTypes = {
+  starships: PropTypes.arrayOf(PropTypes.object)
 }

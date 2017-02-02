c9b699aeaa272f05146a4fa06bd01b3d9185b55c
Melanie Burger <melanie.brgr@gmail.com>
2017-02-01 19:43:32 -0500

    Search tests

---
+++ src/components/Search/Search.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { expect, assert } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import Search from './Search';
+
+describe('<Search />', () => {
+
+  it('should create a search field', () => {
+    const wrapper = shallow(<Search />);
+    
+    assert.isOk(wrapper.children().length, 'Search not found');
+    expect(wrapper.find('input')).to.have.length(1);
+  });
+
+  it('should display a message if there are no starships', () => {
+    const wrapper = shallow(<Search />);
+
+    expect(wrapper.find('div').find('p').text()).to.equal('Nothing here yet...');
+  });
+
+  it('should respond to click events', () => {
+    const searchForStarship = sinon.spy();
+    const wrapper = shallow(<Search searchForStarship={searchForStarship}  />);
+    
+    wrapper.find('button').simulate('click');
+    assert.isTrue(searchForStarship.calledOnce, 'searchForStarship not called');
+    wrapper.find('button').simulate('click');
+    assert.isTrue(searchForStarship.calledTwice, 'searchForStarship not called twice');
+  });  
+
+});
